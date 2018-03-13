83c139433f1fd4ef7d006e7cf8645b3a94a3e944
Przemek Mazur <Przemek1713@gmail.com>
2018-03-14 00:22:25 +0100

    add promise

--- components/App.js
+++ components/App.js
@@ -8,36 +8,44 @@ App = React.createClass({
         };
     },
 
-    handleSearch: function(searchingText) {  
+    handleSearch: function(searchingText) {
         this.setState({
-            loading: true  
-        });
-        this.getGif(searchingText, function(gif) {  
-            this.setState({  
-                loading: false,  
-                gif: gif,  
-                searchingText: searchingText  
-          });
-        }.bind(this));
-      },
+            loading: true
+        }); 
+        this.getGif(searchingText)
+            .then(gif => {
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText          
+                });
+            })
+            .catch(error => console.log('Error', error));
+    },
 
-    getGif: function(searchingText, callback) {  
+    getGif: function(searchingText) {  
         var GIPHY_PUB_KEY = 'vL0XZUL2TlbhD3wH2W27UOWvAkT4xIX5';
         var GIPHY_API_URL = 'https://api.giphy.com';
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-        var xhr = new XMLHttpRequest();.
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data; 
-                var gif = {  
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
+
+        return new Promise (
+            function(resolve, reject) {
+                var request = new XMLHttpRequest();
+                request.onload = function () {
+                    if (this.status === 200) {
+                        resolve(gif); 
+                    } else {
+                        reject(new Error(this.statusText));
+                    }
                 };
-                callback(gif);  
+                request.onerror = function () {
+                    reject(new Error(
+                       `XMLHttpRequest Error: ${this.statusText}`));
+                };
+                request.open('GET', url);
+                request.send();
             }
-        };
-        xhr.send();
+        );
     },  
     
     render: function() {
@@ -50,15 +58,14 @@ App = React.createClass({
 
         return (
           <div style={styles}>
-              <Gif
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
-            <Gif />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />                
           </div>
         );
     }
